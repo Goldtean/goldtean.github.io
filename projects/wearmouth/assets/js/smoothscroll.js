600fe75cab5430350a1bf932514839fab45208c4
Goldtean <jeremyoltean@gmail.com>
2016-05-16 22:55:53 -0600

    Video background and Wearmouth beta

--- projects/wearmouth/assets/js/smoothscroll.js
+++ projects/wearmouth/assets/js/smoothscroll.js
@@ -31,3 +31,33 @@ a=b.extend({},c.defaults,a);return a.lazy?this.bind(a.event,function(d){var c=b(
 
 // Initialize all .smoothScroll links
 jQuery(function($){ $.localScroll({filter:'.smoothScroll'}); });
+
+
+// Pause Button
+
+var vid = document.getElementById("bgvid");
+var pauseButton = document.querySelector("#headerwrap button");
+
+function vidFade() {
+  vid.classList.add("stopfade");
+}
+
+vid.addEventListener('ended', function()
+{
+// only functional if "loop" is removed 
+vid.pause();
+// to capture IE10
+vidFade();
+}); 
+
+
+pauseButton.addEventListener("click", function() {
+  vid.classList.toggle("stopfade");
+  if (vid.paused) {
+    vid.play();
+    pauseButton.innerHTML = "Pause Video";
+  } else {
+    vid.pause();
+    pauseButton.innerHTML = "Play Video";
+  }
+})
